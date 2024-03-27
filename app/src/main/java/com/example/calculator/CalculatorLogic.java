b9d3c30cd96cb38af549c5b1e3555280ae758986
NorbertMarcinkowski <norbert.marcinkowski@protonmail.com>
2024-03-27 20:19:46 +0100

    logic doesn't work yet

--- app/src/main/java/com/example/calculator/CalculatorLogic.java
+++ app/src/main/java/com/example/calculator/CalculatorLogic.java
@@ -1,15 +1,64 @@
 package com.example.calculator;
 
+
+enum Operation {
+    add, subtract, multiply, divide, none
+}
+
 public class CalculatorLogic {
-    String something = "";
+    String expression = "";
+    String firstNumber = "";
+    Operation lastOperation;
+    String secondNumber = "";
     public CalculatorLogic() {
     }
 
     public String logic(String value) {
         if (value.equals("AC")){
-            something = "";
-            return something;
+            expression = "";
+            firstNumber = "";
+            secondNumber = "";
+            return expression;
         }
-        return something += value;
+        if (value.equals("+")){
+            if (expression.isEmpty() && firstNumber.isEmpty()) {
+                return "";
+            }
+            if (!expression.isEmpty() && firstNumber.isEmpty()){
+                firstNumber = expression;
+                expression = "";
+                lastOperation = Operation.add;
+                return firstNumber;
+            }
+            if (!expression.isEmpty()) {
+                float number1 = Float.parseFloat(firstNumber);
+                float number2 = Float.parseFloat(expression);
+                firstNumber = String.valueOf(number1 + number2);
+                expression = "";
+                return firstNumber;
+            }
+
+        }
+        if (value.equals("=")){
+            if (lastOperation == Operation.add) {
+                if (!firstNumber.isEmpty() && !expression.isEmpty()) {
+                    float number1 = Float.parseFloat(firstNumber);
+                    float number2 = Float.parseFloat(expression);
+                    String res = String.valueOf(number1 + number2);
+                    firstNumber = res;
+                    expression = "";
+                    lastOperation = Operation.none;
+                    return res;
+                }
+            }
+            if (!firstNumber.isEmpty()) {
+                return firstNumber;
+            }
+            return "";
+        }
+        if (expression.contains(".") && value.equals(".")) {
+            return expression;
+        }
+        return expression += value;
     }
 }
